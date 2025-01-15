425ad94cec0d966e3562ea395a6e67c558ab883c
Omar Lengua <omarns21@gmail.com>
2025-01-15 01:39:20 +0100

    Implement: exercise 3 completed

--- index.py
+++ index.py
@@ -1,2 +1,6 @@
 # Delete your number variable and its value. Then, declare another variable called text and assign the string 'Hello World' to this variable.
 text = 'Hello World'
+
+# For now, go to a new line and add an empty call to the print() function. You should not see any output yet.
+
+print()
