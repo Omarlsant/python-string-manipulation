3e14f1a53647a164020b832f623ff014af3c22bc
Omar Lengua <omarns21@gmail.com>
2025-01-15 02:21:35 +0100

    Implement: exercise 10 completed

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
-# Now go to a new line and create another variable called shift and assign the value 3 to this variable.
+# And now print your new variable.
 
 text = 'Hello World'
 print(type(text))
 shift = 3
+print(shift)
