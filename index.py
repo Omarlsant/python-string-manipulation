2079db0e492e69b7eb3ecc169f935695467ec89c
Omar Lengua <omarns21@gmail.com>
2025-01-17 19:27:19 +0100

    Implement: exercise 16 completed

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
-# Now assign alphabet.find(text[0]) to a variable named index. In this way, index will store the value returned by alphabet.find(text[0]).
+# Next, print the index variable to the console.
 
 text = 'Hello World'
 shift = 3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 index = alphabet.find(text[0])
+print(index)
