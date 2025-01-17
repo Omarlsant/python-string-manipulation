c60b15acef3f5c07566d3b1dd33c0b68d911a399
Omar Lengua <omarns21@gmail.com>
2025-01-17 19:26:10 +0100

    Implement: exercise 15 completed

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
-# To implement this, you will use the .find() method discussed in the previous step. Modify your existing .find() call passing it text[0] as the argument instead of 'z'.
+# Now assign alphabet.find(text[0]) to a variable named index. In this way, index will store the value returned by alphabet.find(text[0]).
 
 text = 'Hello World'
 shift = 3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
-alphabet.find(text[0])
+index = alphabet.find(text[0])
