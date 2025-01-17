ffa0514a607fda347f7e141cf63c4c719ccc40ff
Omar Lengua <omarns21@gmail.com>
2025-01-17 19:24:23 +0100

    Implement: exercise 14 completed

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
-# At the end of your code, call .find() on alphabet and pass 'z' as the argument to the method.
+# To implement this, you will use the .find() method discussed in the previous step. Modify your existing .find() call passing it text[0] as the argument instead of 'z'.
 
 text = 'Hello World'
 shift = 3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
-alphabet.find('z')
+alphabet.find(text[0])
