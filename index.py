5ab8e6444184fa38316732769c31571e919d4953
Omar Lengua <omarns21@gmail.com>
2025-01-17 19:22:35 +0100

    Implement: exercise 13 completed

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
-# Remove both calls to print() and declare another variable called alphabet. Assign the string 'abcdefghijklmnopqrstuvwxyz' to this variable.
+# At the end of your code, call .find() on alphabet and pass 'z' as the argument to the method.
 
 text = 'Hello World'
 shift = 3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
+alphabet.find('z')
