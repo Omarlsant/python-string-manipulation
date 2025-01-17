8f43b45e814fbdd7d9cb4e54d9aff0c6b51e2c00
Omar Lengua <omarns21@gmail.com>
2025-01-17 19:32:31 +0100

    Implement: exercise 17 completed

--- index.py
+++ index.py
@@ -1,7 +1,8 @@
-# Next, print the index variable to the console.
+# You can transform a string into its lowercase equivalent with the .lower() method. Add another print() call to print text.lower() and see the output.
 
 text = 'Hello World'
 shift = 3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 index = alphabet.find(text[0])
 print(index)
+print(text.lower())
