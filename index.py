e8a009ed9d7cd69a58619002714a1cf6251f2844
Omar Lengua <omarns21@gmail.com>
2025-01-19 10:47:06 +0100

    Implement: exercise 24 completed

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
-# Step 23: Below the line where you declared alphabet, write a for loop to iterate over text. Use i as the loop variable.
+# Step 24: Give your for loop a body by adding a call to print(i). Remember to indent the loop body.
 
 text = 'Hello World'
 shift = 3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 for i in text:
+    print(i)
