3f5999efb20ac5e269269aa786f979a152817a96
Omar Lengua <omarns21@gmail.com>
2025-01-19 22:23:44 +0100

    Implement: exercise 28 completed

--- index.py
+++ index.py
@@ -1,8 +1,8 @@
-# Step 27: Add a second argument to print(char) so that it prints the character and its index inside the alphabet.
+# Step 28: find is again returning -1 for uppercase letters, and for the space character, too. For now, instead of iterating over text, change the for loop to iterate over text.lower().
 
 text = 'Hello World'
 shift = 3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
-for char in text:
+for char in text.lower():
     index = alphabet.find(char)
     print(char, index)
