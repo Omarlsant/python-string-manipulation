3295ef0006d729c517af6ebc4e8a0c15c09b72a6
Omar Lengua <omarns21@gmail.com>
2025-01-19 22:18:51 +0100

    Implement: exercise 26 completed

--- index.py
+++ index.py
@@ -1,7 +1,8 @@
-# Step 25: Rename your i variable to char.
+# Step 26: Inside the for loop, before printing the current character, declare a variable called index and assign the value returned by alphabet.find(char) to this variable.
 
 text = 'Hello World'
 shift = 3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 for char in text:
+    index = alphabet.find(char)
     print(char)
