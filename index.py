35e01222f5db1b697377cf111da02e2aeb0843ab
Omar Lengua <omarns21@gmail.com>
2025-01-19 10:33:33 +0100

    Implement: exercise 23 completed

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
-# Step 22: For now, remove all the lines of code below the declaration of the alphabet variable.
+# Step 23: Below the line where you declared alphabet, write a for loop to iterate over text. Use i as the loop variable.
 
 text = 'Hello World'
 shift = 3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
+for i in text:
