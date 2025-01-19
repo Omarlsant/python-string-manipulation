58ecd95a7c2574774a213f28976603938e786a98
Omar Lengua <omarns21@gmail.com>
2025-01-19 10:29:08 +0100

    Implement: exercise 21 completed

--- index.py
+++ index.py
@@ -1,9 +1,9 @@
-# Step 20: Print your shifted variable.
+# Step 21: Modify your shifted variable so that it stores the value of alphabet at index index + shift.
 
 text = 'Hello World'
 shift = 3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 index = alphabet.find(text[0].lower())
 print(index)
-shifted = alphabet[index]
+shifted = alphabet[index + shift]
 print(shifted)
