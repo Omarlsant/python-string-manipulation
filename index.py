464813f6aca52d8c1c2313eb1b559fdba9322562
Omar Lengua <omarns21@gmail.com>
2025-01-21 18:20:04 +0100

    Implement: exercise 33 completed

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-# Step 32: Go back to the original string by deleting the text reassignment.
+# Step 33: Now you need to create a new_char variable at the end of your loop body. Set its value to alphabet[new_index].
 
 text = 'Hello World'
 shift = 3
@@ -7,3 +7,4 @@ for char in text.lower():
     index = alphabet.find(char)
     print(char, index)
     new_index = index + shift
+    new_char = alphabet[new_index]
