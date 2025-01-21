49d9ed4583c2ac46edc7bc711243112b1b1706f1
Omar Lengua <omarns21@gmail.com>
2025-01-21 18:28:19 +0100

    Implement: exercise 35 completed

--- index.py
+++ index.py
@@ -1,11 +1,10 @@
-# Step 34: Next, print new_char and see the output.
+# Step 35: Clean the output a bit. Delete print(char, index), and turn the last print() call into print('char:', char, 'new char:', new_char).
 
 text = 'Hello World'
 shift = 3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 for char in text.lower():
     index = alphabet.find(char)
-    print(char, index)
     new_index = index + shift
     new_char = alphabet[new_index]
-    print(new_char)
+    print('char:', char, 'new char:', new_char)
