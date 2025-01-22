6f15ec83d36ac119d9cecd19c439b8695264a293
Omar Lengua <omarns21@gmail.com>
2025-01-22 14:09:02 +0100

    Implement: exercise 41 completed

--- index.py
+++ index.py
@@ -1,11 +1,12 @@
-# Step 40: At the beginning of your loop body, print the result of comparing char with a space (' '). Use the equality operator == for that.
+# Step 41: At the top of your for loop, replace print(char == ' ') with an if statement. The condition of this if statement should evaluate to True if char is an empty space and False otherwise. Inside the if body, print the string 'space!'. Remember to indent this line.
 
 text = 'Hello World'
 shift = 3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 encrypted_text = ''
 for char in text.lower():
-    print(char == ' ')
+    if char == ' ':
+        print('space!')
     index = alphabet.find(char)
     new_index = index + shift
     encrypted_text += alphabet[new_index]
