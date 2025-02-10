93e597cc3d43643d27c5691820ea36394ae24089
Omar Lengua <omarns21@gmail.com>
2025-02-10 18:16:49 +0100

    Implement: exercise 54 completed

--- index.py
+++ index.py
@@ -1,18 +1,18 @@
-# Step 53: Modify your function declaration so that it takes two parameters called message and offset. After that, you'll see an error appear in the terminal. 
+# Step 54: Inside your function body, rename the text and shift variables to message and offset, respectively.
 
 text = 'Hello Zaira'
 shift = 3
 def caesar(message, offset):
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     encrypted_text = ''
-    for char in text.lower():
+    for char in message.lower():
         if char == ' ':
             encrypted_text += char
         else:
             index = alphabet.find(char)
-            new_index = (index + shift) % len(alphabet)
+            new_index = (index + offset) % len(alphabet)
             encrypted_text += alphabet[new_index]
-    print('plain text:', text)
+    print('plain text:', message)
     print('encrypted text:', encrypted_text)
 
 caesar()
