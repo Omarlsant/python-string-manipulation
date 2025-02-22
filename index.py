c3af9b267b001d53a7106880736acf502a23ffa9
Omar Lengua <omarns21@gmail.com>
2025-02-22 13:30:00 +0100

    Implement: exercise 67 completed

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-# Step 66: Above the offset variable, create another comment saying Define the offset and the encrypted letter.
+# Step 67: Remove the two print() calls from your function and return encrypted_text.
 
 text = 'Hello Zaira'
 custom_key = 'python'
@@ -19,6 +19,4 @@ def vigenere(message, key):
             index = alphabet.find(char)
             new_index = (index + offset) % len(alphabet)
             encrypted_text += alphabet[new_index]
-
-    print('plain text:', message)
-    print('encrypted text:', encrypted_text)
+    return encrypted_text
