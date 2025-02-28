54cba6c116a2e1f6ba8f3529252f959109ea3e54
Omar Lengua <omarns21@gmail.com>
2025-02-28 22:46:05 +0100

    Implement: exercise 81 completed

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-# Step 80: Delete the condition char == ' ' and replace it by calling the .isalpha() method on char.
+# Step 81: The not operator is used to negate an expression. When placed before a truthy value — a value that evaluates to True — it returns False and vice versa. Add the not operator at the beginning of the if condition to check if the character is not alphabetic.
 
 text = 'Hello Zaira!'
 custom_key = 'python'
@@ -8,7 +8,7 @@ def vigenere(message, key, direction=1):
     final_message = ''
     for char in message.lower():
         # Append space to the message
-        if char.isalpha():
+        if not char.isalpha():
             final_message += char
         else:
         # Find the right key character to encode/decode
