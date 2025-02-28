73b1de3922b1a3972f0b78f4a733a20bf3d1791e
Omar Lengua <omarns21@gmail.com>
2025-02-28 22:21:05 +0100

    Implement: exercise 71 completed

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-# Step 70: Encryption and decryption are opposite processes and your function can do both with a couple of tweaks. Add a third parameter called direction to your function definition. Also, comment out the last two lines of code to avoid errors in the console.
+# Step 71: All you need to do is multiply the offset by the direction in the new_index assignment. The multiplication operator in Python is *.
 
 text = 'Hello Zaira'
 custom_key = 'python'
@@ -17,7 +17,7 @@ def vigenere(message, key, direction):
             # Define the offset and the encrypted letter
             offset = alphabet.index(key_char)
             index = alphabet.find(char)
-            new_index = (index + offset) % len(alphabet)
+            new_index = (index + offset * direction) % len(alphabet)
             encrypted_text += alphabet[new_index]
     return encrypted_text
 
